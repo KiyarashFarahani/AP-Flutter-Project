9c79244eb904873eb3a1ccb51637affa2aa37b76
kiyarash <farahanikiyarash@gmail.com>
2025-05-14 21:53:18 +0330

    feat: implement equals() and hashCode() for Playlist class

--- Playlist.java
+++ Playlist.java
@@ -50,4 +50,16 @@ public class Playlist {
         return songs;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Playlist playlist = (Playlist) o;
+        return Objects.equals(id, playlist.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
