d1ababdaac3cc54cd225f61281db302c1e554a42
Ramtin <ramtin.badri01@gmail.com>
2025-06-10 22:57:38 +0330

    feat: add java response model class

---
+++ backend/dto/Response.java
@@ -0,0 +1,42 @@
+package backend.dto;
+
+public class Response<T> {
+    private String status;
+    private T data;
+    private String message;
+    private int httpCode;
+
+    public Response() {}
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public int getHttpCode() {
+        return httpCode;
+    }
+
+    public void setHttpCode(int httpCode) {
+        this.httpCode = httpCode;
+    }
+}
