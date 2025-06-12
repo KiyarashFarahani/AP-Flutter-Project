c83656839f0040849231f6a9b3314476e6ad250b
Ramtin <ramtin.badri01@gmail.com>
2025-06-13 00:30:16 +0330

    fix: change playlistId field type into int

--- backend/dto/SharePlaylistData.java
+++ backend/dto/SharePlaylistData.java
@@ -6,11 +6,11 @@ public class SharePlaylistData {
     @SerializedName("recipient_username")
     private String recipientUsername;
     @SerializedName("playlist_id")
-    private String playListId;
+    private int playListId;
 
     public SharePlaylistData() {}
 
-    public SharePlaylistData(String recipientUsername, String playListId) {
+    public SharePlaylistData(String recipientUsername, int playListId) {
         this.recipientUsername = recipientUsername;
         this.playListId = playListId;
     }
@@ -23,11 +23,11 @@ public class SharePlaylistData {
         this.recipientUsername = recipientUsername;
     }
 
-    public String getPlayListId() {
+    public int getPlayListId() {
         return playListId;
     }
 
-    public void setPlayListId(String playListId) {
+    public void setPlayListId(int playListId) {
         this.playListId = playListId;
     }
 }
