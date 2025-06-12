fc1a4a25390e038b7d1f7f15ae05e018cbfb8c38
Ramtin <ramtin.badri01@gmail.com>
2025-06-13 01:10:03 +0330

    fix: add serialized names for Theme enum

--- backend/model/Theme.java
+++ backend/model/Theme.java
@@ -1,6 +1,10 @@
 package backend.model;
 
+import com.google.gson.annotations.SerializedName;
+
 public enum Theme {
+    @SerializedName("dark")
     DARK,
+    @SerializedName("light")
     LIGHT
 }
