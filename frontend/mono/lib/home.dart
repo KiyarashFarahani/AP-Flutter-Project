20c6854cd5619502ec61aff77617ab5830c931b9
kiyarash <farahanikiyarash@gmail.com>
2025-07-26 15:39:16 +0330

    feat: link login to home

---
+++ frontend/mono/lib/home.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'dart:ui';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
